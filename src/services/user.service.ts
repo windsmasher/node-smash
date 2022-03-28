e1b424bdcf76e1d34afa928b5ff4e2be5d4faf18
Tomasz Paczka <tpaczka@future-processing.com>
2022-03-28 10:14:27 +0200

    Some fixes.

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,45 +8,56 @@ import * as userRepository from '../repositories/user.repository';
 
 export const userRegisterMiddleware =
   () => async (req: Request, res: Response, next: NextFunction) => {
-    if (await userRepository.isUserExists(req.body.email)) {
-      return next(new HttpException(500, 'User exists.'));
+    try {
+      if (await userRepository.isUserExists(req.body.email)) {
+        return next(new HttpException(500, 'User exists.'));
+      }
+      const salt = await bcrypt.genSalt(10);
+      const hashedPwd = await bcrypt.hash(req.body.password, salt);
+
+      const savedUser = await userRepository.createAndSaveUser({
+        email: req.body.email,
+        password: hashedPwd,
+      });
+      res.send({ id: savedUser.id, email: savedUser.email });
+    } catch (err) {
+      next(err);
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPwd = await bcrypt.hash(req.body.password, salt);
-
-    const savedUser = await userRepository.createAndSaveUser({
-      email: req.body.email,
-      password: hashedPwd,
-    });
-    res.send({ id: savedUser.id, email: savedUser.email });
   };
 
 export const userLoginMiddleware =
   () => async (req: Request, res: Response, next: NextFunction) => {
-    const user = await userRepository.findUserByEmail(req.body.email);
-    if (!user) {
-      return next(new WrongLoginException());
-    }
+    try {
+      const user = await userRepository.findUserByEmail(req.body.email);
+      if (!user) {
+        return next(new WrongLoginException());
+      }
 
-    if (!(await bcrypt.compare(req.body.password, user.password))) {
-      return next(new WrongLoginException());
-    }
+      if (!(await bcrypt.compare(req.body.password, user.password))) {
+        return next(new WrongLoginException());
+      }
 
-    const secret = process.env.JWT_SECRET;
-    if (!secret) {
-      return next(new HttpException(500, 'There is no secret for jwt.'));
-    }
-    const token = jwt.sign({ email: user.email, id: user._id }, secret);
+      const secret = process.env.JWT_SECRET;
+      if (!secret) {
+        return next(new HttpException(500, 'There is no secret for jwt.'));
+      }
+      const token = jwt.sign({ email: user.email, id: user._id }, secret);
 
-    res.send({ token });
+      res.send({ token });
+    } catch (err) {
+      next(err);
+    }
   };
 
 export const userDetailsMiddleware =
   () => async (req: RequestWithUser, res: Response, next: NextFunction) => {
-    const user = await userRepository.findUserById(req?.user?.id);
-    if (!user) {
-      return next(new HttpException(500, ''));
+    try {
+      const user = await userRepository.findUserById(req?.user?.id);
+      if (!user) {
+        return next(new HttpException(500, 'User does not exists.'));
+      }
+      res.send({ id: user.id, email: user.email });
+    } catch (err) {
+      next(err);
     }
-    res.send({ id: user.id, email: user.email });
   };
